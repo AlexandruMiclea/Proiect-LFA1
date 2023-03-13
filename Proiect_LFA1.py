5196a7a9d695fab4f548f39df2eb4b3d64e506bc
Alexandru Miclea <alexandrumiclea@outlook.com>
2023-03-13 20:51:35 +0200

    gata tema what? ramane sa mai fac cateva teste

--- Proiect_LFA1.py
+++ Proiect_LFA1.py
@@ -1,5 +1,4 @@
-from queue import Empty
-
+import copy
 
 class Automat:
 
@@ -14,6 +13,9 @@ class Automat:
         self.stariFinale = list()
         self.cuvinte = list()
         self.NFA = False
+        self.solutii = list()
+
+        self.drumCurent = list()
 
     def readAutomat(self, fisier):
 
@@ -24,31 +26,25 @@ class Automat:
             self.matriceTranzitii = [[list() for x in range(len(self.stari))] for i in range(len(self.stari))] 
             #print(matriceTranzitii)
 
-            alfabet = [x for x in fisierautomat.readline().strip('\n')]
+            self.alfabet = [x for x in fisierautomat.readline().strip('\n')]
             self.aux = fisierautomat.readlines()
             for i in range(len(self.aux) - 2):
                 l = [x for x in self.aux[i].strip('\n').split()]
                 #print(stari.index(l[0]))
                 #print(stari.index(l[2]))
 
-
+                # verificam daca avem de-a face cu nfa sau dfa
                 for linie in self.matriceTranzitii[self.stari.index(l[0])]:
                     for caseta in linie:
                         if l[1] in caseta:
-                            NFA = True
-                self.matriceTranzitii[self.stari.index(l[0])][self.stari.index(l[2])].append(l[1])
-        
-        
-                #print(l)
-            #tranzitii = [((x[0],x[1]), x[2]) for x in aux[:-2]]
-            #print(tranzitii)
+                            self.NFA = True
 
+                self.matriceTranzitii[self.stari.index(l[0])][self.stari.index(l[2])].append(l[1])
 
             self.stareInit = self.aux[-2].strip('\n')
             self.stariFinale = [x for x in self.aux[-1].split()]
 
-        #citim cuvintele, avand grija sa citim si cuvantul nul
-
+    #citim cuvintele, avand grija sa citim si cuvantul nul
     def readCuvinte(self, fisier):
 
         with open(fisier,"r") as fisiercuv:
@@ -56,8 +52,7 @@ class Automat:
             for cuv in aux:
                 self.cuvinte.append(cuv.strip('\n'))
         
-    # verificam daca avem de-a face cu nfa sau dfa
-
+    #afisam datele dupa citire
     def printData(self):
 
         print(self.stari)
@@ -72,23 +67,61 @@ class Automat:
                 print(j, end=' ')
             print()
 
+
+    def DFS(self, nod, cuvant):
+
+        self.drumCurent.append(nod)
+        cDrumCurent = copy.deepcopy(self.drumCurent)
+        stariNoi = list()
+
+        if cuvant:
+
+            litera = cuvant[0]
+            cuvant = cuvant[1:]
+
+            for i in range(len(self.matriceTranzitii[self.stari.index(nod)])):
+                if litera in self.matriceTranzitii[self.stari.index(nod)][i]:
+                    stariNoi.append(self.stari[i])
+
+            for el in stariNoi:
+                self.DFS(el, cuvant)
+                self.drumCurent = cDrumCurent
+
+        else:
+            if nod in self.stariFinale:
+                self.solutii.append(self.drumCurent)
+
+        #adauga nodul curent in lista
+        #vezi unde te poti duce cu prima litera pe graf
+            #daca nu mai sunt litere vezi daca esti stare finala
+                #daca da, da append listei la raspunsuri
+            #daca mai sunt litere
+            #dfs pe lista de litere
+        #
+
+
+        return
+
     def checkWords(self):
+
+        tipAutomat = "AFN" if self.NFA else "AFD"
+        print(f"Aici avem un {tipAutomat}.")
 
         for word in self.cuvinte:
 
-            lStari = list()
-            lStari.append(self.stareInit)
+            self.solutii = list()
+            self.drumCurent = list()
 
-            while lStari is not Empty:
-                for litera in word:
-                    for vStare in lStari:
-                        newStari = list()
-                        for caseta in self.matriceTranzitii[self.stari.index(vStare)]:
-                            if 
+            self.DFS(self.stareInit, word)
+            
+            if self.solutii:
+                print(f"Automatul acesta este finit pentru cuvantul {word}!")
+                print("Drumurile cu care ajungem in stare finita sunt:")
+                for lista in self.solutii:
+                    print(*lista)
+            else:
+                print(f"Automatul acesta NU este finit pentru cuvantul {word}!")
 
-        stareCurenta = self.stareInit
-        
-        
 
 
 
@@ -97,4 +130,5 @@ if __name__ == "__main__":
     x.readAutomat("automat.txt")
     x.readCuvinte("cuvinte.txt")
     x.printData()
+    x.checkWords()
 
